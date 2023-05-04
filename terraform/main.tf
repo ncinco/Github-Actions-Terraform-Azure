3236a17f6e977200ec8fb4875fa3e4d74d62736d
ncinco <32296929+ncinco@users.noreply.github.com>
2023-05-04 23:06:50 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
 terraform {
- required_version = ">= 1.0.0"
- required_providers {
-   astra = {
-     source = "datastax/astra"
-     version = ">=1.0.0"
-   }
- }
+  required_providers {
+    astra = {
+      source = "datastax/astra"
+      version = ">=1.0.0"
+    }
+  }
 }
